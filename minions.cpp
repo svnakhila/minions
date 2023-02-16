2aafaf813aab626dd052a584d7998534ad43e1d5
svnakhila <122998079+svnakhila@users.noreply.github.com>
2023-02-15 21:42:13 -0500

    Update minions.cpp

--- minions.cpp
+++ minions.cpp
@@ -4,34 +4,36 @@
 using namespace std;
 
 void print(int x)
+
 {
-cout << "Hello! I am minion " << x << end1;
+        cout << "Hello! I am minion " << x << endl;
 }
 
 
-int main (int argc, char** argv) {
-  if (argc < 2) {
-    std::cerr<<"usage: "<<argv[0]<<" <nbminions>\n";
-    return -1;
-  }
-  //Your code goes here
-
-int n = atoi(argv[1]) ;
-
-std:: thread mythreads[n];
-
-for(int i=o;i<n;i++)
-
+int main (int argc, char **argv)
 {
-mythreads[i] = std::thread(print,i+1);
-}
+        if (argc < 2)
+        {
+                std::cerr<<"usage: "<<argv[0]<<" <nbminions>\n";
+                return -1;
+        }
 
-for(int i=0;i<n;i++)
-{
-mythreads[i].join();
-}
+        int n = atoi(argv[1]);
 
-cout << "hello minions! I am the Overlord!" << end1;
-  
-  return 0;
+        std::thread mythreads[n];
+
+        for(int i=0;i<n;i++)
+
+        {
+                mythreads[i] = std::thread(print,i+1);
+        }
+
+        for(int i=0;i<n;i++)
+        {
+                mythreads[i].join();
+        }
+
+        cout << "hello minions! I am the Overlord!" << endl;
+
+        return 0;
 }
